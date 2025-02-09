617b75138d5369647b54f473a026cd07afe2f49f
parswork <hassan.ele.ghabel@gmail.com>
2025-02-09 09:43:22 -0500

    new update

--- dore1/urls.py
+++ dore1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import Views
+app=Views()
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', app.mainPage),
 ]
--- dore1/views.py
+++ dore1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from backend.data_sent import data_sent
+from .backend.data_sent import data_sent
 import os
 from django.http import HttpResponse, Http404
 import mimetypes
@@ -9,7 +9,6 @@ import mimetypes
 class Views():
     def __init__(self) -> None:
         self.data_sent=data_sent()
- 
     def mainPage(self,request):
-        return render(request, 'home.html')
+        return render(request, 'index.html')
     
